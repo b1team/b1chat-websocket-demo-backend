1df8a15aa429aa288ba62eda1ce0f857d153e537
Le Van Vuong <vuonglevan@vccorp.vn>
2020-07-04 08:28:57 +0700

    beta

--- src/main/java/chatroom/models/UserDecoder.java
+++ src/main/java/chatroom/models/UserDecoder.java
@@ -8,44 +8,40 @@ import javax.websocket.DecodeException;
 import javax.websocket.Decoder;
 import javax.websocket.EndpointConfig;
 
-public class UserDecoder implements Decoder.Text<User>{
+public class UserDecoder implements Decoder.Text<User> {
 
 	@Override
 	public void init(EndpointConfig config) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public User decode(String jsonMessage) throws DecodeException {
-		JsonObject jsonObject = Json
-		        .createReader(new StringReader(jsonMessage)).readObject();
-			User user = new User();
-			
-			user.setUsername(jsonObject.getString("username"));
-			user.setPassword(jsonObject.getString("password"));
-			user.setRaw(jsonMessage);
-			return user;
+		JsonObject jsonObject = Json.createReader(new StringReader(jsonMessage)).readObject();
+		User user = new User();
+
+		user.setUsername(jsonObject.getString("username"));
+		user.setPassword(jsonObject.getString("password"));
+		user.setRaw(jsonMessage);
+		return user;
 	}
 
 	@Override
 	public boolean willDecode(String jsonMessage) {
 		try {
-		      // Check if incoming message is valid JSON
-		      Json.createReader(new StringReader(jsonMessage)).readObject();
-		      return true;
-		    } catch (Exception e) {
-		      return false;
-		    }
-		  }
+			// Check if incoming message is valid JSON
+			Json.createReader(new StringReader(jsonMessage)).readObject();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 
-	
-	
 }
-
